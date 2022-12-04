819412be205e1706f58885f6cc378494f96346ef
Rosendo <ewerton.rosendo15@gmail.com>
2022-12-04 16:23:22 -0300

    dale

---
+++ src/ArvoreAVL/src/arvoreavl/Arvore.java
@@ -0,0 +1,121 @@
+package arvoreavl;
+
+public class Arvore {
+
+    private No noraiz;
+
+    public Arvore(No noraiz) {
+        this.noraiz = noraiz;
+    }
+
+    public boolean isEmpty() {
+        if (noraiz == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void imprimirArvore() {
+        if (this.noraiz == null) {
+            System.out.println("Árvore vazia");
+        } else {
+            imprimirArvore(this.noraiz);
+        }
+    }
+
+    private void imprimirArvore(No node) {
+        if (node.getNoEsquerda() != null) {
+            imprimirArvore(node.getNoEsquerda());
+        }
+        if (node.getNoDireita() != null) {
+            imprimirArvore(node.getNoDireita());
+        }
+        System.out.print(node.getValor() + "-");
+        try {
+            System.out.println("NóEsquerda: " + node.getNoEsquerda().getValor());
+            System.out.println("NóDireita: " + node.getNoDireita().getValor());
+        } catch (NullPointerException e) {
+            e.getStackTrace();
+        }
+    }
+
+    public void inserir(int valor) {
+        inserir(this.noraiz, valor);
+    }
+
+    public void inserir(No node, int valor) {
+
+        //alterar o método para uma árvore AVL
+        if (this.noraiz == null) {
+            this.noraiz = new No(null, valor);
+        } else {
+            if (valor < node.getValor()) {
+                if (node.getNoEsquerda() != null) {
+                    inserir(node.getNoEsquerda(), valor);
+                } else {
+                    //Se nodo esquerdo vazio insere o novo no aqui 
+                    node.setNoEsquerda(new No(node, valor));
+                }
+                //Verifica se o valor a ser inserido é maior que o no corrente da árvore, se sim vai para subarvore direita 
+            } else if (valor > node.getValor()) {
+                //Se tiver elemento no no direito continua a busca 
+                if (node.getNoDireita() != null) {
+                    inserir(node.getNoDireita(), valor);
+                } else {
+                    //Se nodo direito vazio insere o novo no aqui 
+                    node.setNoDireita(new No(node, valor));
+                }
+            }
+        }
+    }
+
+    public No remover(int valor) throws Exception {
+        return remover(this.noraiz, valor);
+    }
+
+    private No remover(No node, int valor) {
+
+        //alterar o método para uma árvore AVL
+        
+        if (this.noraiz == null) {
+            System.out.println("Árvore vazia");
+        } else {
+            if (valor < node.getValor()) {
+                node.setNoEsquerda(remover(node.getNoEsquerda(), valor));
+            } else if (valor > node.getValor()) {
+                node.setNoDireita(remover(node.getNoDireita(), valor));
+            } else if (node.getNoEsquerda() != null && node.getNoDireita() != null) {
+                /*2 filhos*/
+                System.out.println("  Removeu No " + node.getValor());
+                node.setValor(encontraMinimo(node.getNoDireita()).getValor());
+                node.setNoDireita(removeMinimo(node.getNoDireita()));
+            } else {
+                System.out.println("  Removeu No " + node.getValor());
+                node = (node.getNoEsquerda() != null) ? node.getNoEsquerda() : node.getNoDireita();
+            }
+
+        }
+        return node;
+    }
+
+    private No removeMinimo(No node) {
+        if (node == null) {
+            System.out.println("  ERRO ");
+        } else if (node.getNoEsquerda() != null) {
+            node.setNoEsquerda(removeMinimo(node.getNoEsquerda()));
+            return node;
+        } else {
+            return node.getNoDireita();
+        }
+        return null;
+    }
+
+    private No encontraMinimo(No node) {
+        if (node != null) {
+            while (node.getNoEsquerda() != null) {
+                node = node.getNoEsquerda();
+            }
+        }
+        return node;
+    }
+}
---
+++ src/ArvoreAVL/src/arvoreavl/No.java
@@ -0,0 +1,56 @@
+package arvoreavl;
+
+public class No {
+
+    private int valor;
+    private No noPai;
+    private No noEsquerda;
+    private No noDireita;
+
+    No(No noPai, int valor) {
+        this.valor = valor;
+        this.noPai = noPai;
+        this.noEsquerda = null;
+        this.noDireita = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getNoPai() {
+        return noPai;
+    }
+
+    public void setNoPai(No noPai) {
+        this.noPai = noPai;
+    }
+
+    public No getNoEsquerda() {
+        return noEsquerda;
+    }
+
+    public void setNoEsquerda(No noEsquerda) {
+        this.noEsquerda = noEsquerda;
+    }
+
+    public No getNoDireita() {
+        return noDireita;
+    }
+
+    public void setNoDireita(No noDireita) {
+        this.noDireita = noDireita;
+    }
+
+    public No(int valor, No noPai, No noEsquerda, No noDireita) {
+        this.valor = valor;
+        this.noPai = noPai;
+        this.noEsquerda = noEsquerda;
+        this.noDireita = noDireita;
+    }
+
+}
